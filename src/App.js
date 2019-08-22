05934825fbee18311d43d2318a1199db47514aa2
mpang-Lotus <mei@lotusresearch.com>
2019-08-22 13:56:10 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Hello Dear.
+          Edit <code>src/App.js</code> and Hello from afar.
         </p>
         <a
           className="App-link"
